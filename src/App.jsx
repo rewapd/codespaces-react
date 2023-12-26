08815d0422c6ab2d7d97afa1d1a4cae1d8aee4f5
Rewa Prasad <72617566+rewapd@users.noreply.github.com>
2023-12-26 14:24:09 +0000

    currency converter

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,54 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+const [amount,setAmount]=useState("1")
+const [fromCurr,setFromCurr]=useState("USD");
+const [toCurr,setToCurr]=useState("INR")
+const [output,setOutput]=useState("")
+
+function handleAmount(event){
+  setAmount(Number(event.target.value));
+}
+
+function handleFromCurr(event){
+  setFromCurr(event.target.value);
+}
+
+function handleTocurr(event){
+  setToCurr(event.target.value);
+}
+
+useEffect(function(){
+  async function fetchData(){
+    const res= await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurr}&to=${toCurr}`)
+  const data=await res.json()
+      setOutput(data.rates[toCurr])
+  }
+
+  if (fromCurr===toCurr) {
+    return setOutput(amount)
+  }
+  fetchData();
+},[amount,fromCurr,toCurr])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      <input type="text" name="amount" value={amount} id="amnt" onChange={(event)=>handleAmount(event)}/>
+      <select name="currency" id="cuur" value={fromCurr} onChange={(event)=>handleFromCurr(event)}>
+        <option value="USD">USD</option>
+        <option value="INR">INR</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+      </select>
+      <select name="curr" id="toCurr" value={toCurr} onChange={(event)=>handleTocurr(event)} >
+        <option value="USD">USD</option>
+        <option value="INR">INR</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+      </select>
+      <p>{output} {toCurr} </p>
     </div>
   );
 }
